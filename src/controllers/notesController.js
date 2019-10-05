b61627810e6b5b7a895631114c0a61a302631dd6
Elias T <eliastorrealba1998@outlook.com>
2019-10-05 12:52:54 -0400

    Improving error handlers & adding enviroment example

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -7,7 +7,7 @@ exports.getAllNotes = async (req, res, next) => {
         const Notes = await Note.findAll();
         return res.status(200).json(Notes);
     } catch (err) {
-        console.error(err);
+        next(err);
     }
 };
 
@@ -31,7 +31,7 @@ exports.postNotes = async (req, res, next) => {
         if (!err.statusCode) {
             err.statusCode = 500;
         }
-        throw err;
+        next(err);
     }
 };
 
@@ -40,14 +40,16 @@ exports.getNote = async (req, res, next) => {
     try {
         const note = await Note.findByPk(id);
         if (!note) {
-            return res.status(404).end();
+            const error = new Error('Note not found.');
+            error.statusCode = 404;
+            throw error;
         }
         return res.status(202).json(note);
     } catch (err) {
         if (!err.statusCode) {
             err.statusCode = 500;
         }
-        throw err;
+        next(err);
     }
 };
 
@@ -64,27 +66,24 @@ exports.updateNote = async (req, res, next) => {
     try {
         const updatedNote = await Note.findByPk(noteId);
         if (!updatedNote) {
-            return res
-                .status(404)
-                .json({ message: 'Couldnt find post.' })
-                .end();
+            const error = new Error('Note not found.');
+            error.statusCode = 404;
+            throw error;
         }
         if (updatedNote.userId !== req.userId) {
-            return res
-                .status(403)
-                .json({ message: 'Not authorized.' })
-                .end();
+            const error = new Error('Not authorized!.');
+            error.statusCode = 403;
+            throw error;
         }
         updatedNote.title = title;
         updatedNote.body = body;
         await updatedNote.save();
         return res.status(202).json(updatedNote);
     } catch (err) {
-        console.log(err);
         if (!err.statusCode) {
             err.statusCode = 500;
         }
-        throw err;
+        next(err);
     }
 };
 
@@ -93,16 +92,14 @@ exports.deleteNote = async (req, res, next) => {
     try {
         const note = await Note.findByPk(id);
         if (!note) {
-            return res
-                .status(404)
-                .json({ message: 'Couldnt find post.' })
-                .end();
+            const error = new Error('Note not found.');
+            error.statusCode = 404;
+            throw error;
         }
         if (note.userId !== req.userId) {
-            return res
-                .status(403)
-                .json({ message: 'Not authorized.' })
-                .end();
+            const error = new Error('Not authorized.');
+            error.statusCode = 403;
+            throw error;
         }
         await note.destroy();
         return res
@@ -112,6 +109,6 @@ exports.deleteNote = async (req, res, next) => {
         if (!err.statusCode) {
             err.statusCode = 500;
         }
-        throw err;
+        next(err);
     }
 };
